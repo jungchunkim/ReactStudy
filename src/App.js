50c06da234b6be35ed8154f41a1dfef10846144b
jungchunkim <wjdcjs22@naver.com>
2021-09-13 13:47:14 +0900

    use state mode with example

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // read mode , welcome mode
+      mode: "welcome",
       subject: { title: 'WEB', sub: 'World wide web!' },
+      welcome: { title: "Welcome", desc: "Hello, React!!" },
       contents: [
         { id: 1, title: 'HTML', desc: 'HTML is HyperText ...' },
         { id: 2, title: 'CSS', desc: 'CSS is for design' },
@@ -23,6 +26,16 @@ class App extends Component {
     }
   }
   render() {
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+
+
     return (
       <div className="App">
         <Subject
@@ -30,7 +43,7 @@ class App extends Component {
           sub={this.state.subject.sub}>
         </Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="cheon" desc="react prac"></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
 
     );
